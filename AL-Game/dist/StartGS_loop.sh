3674abe7ad4379c1bc9e7c537c015f4c14e87696
luiz.philip@b2wdigital.com <luiz.philip@b2wdigital.com>
2020-09-01 03:40:58 -0300

    implementando geodata e alterando rates para testes

--- AL-Game/dist/StartGS_loop.sh
+++ AL-Game/dist/StartGS_loop.sh
@@ -5,7 +5,7 @@ until [ $err == 0 ];
 do
 	[ -d log/ ] || mkdir log/
 	[ -f log/console.log ] && mv log/console.log "log/backup/`date +%Y-%m-%d_%H-%M-%S`_console.log"
-	java -Xms128m -Xmx1536m -Xbootclasspath/p:libs/jsr166-1.0.0.jar -ea -javaagent:./libs/al-commons-1.3.jar -cp ./libs/*:AL-Game.jar com.aionemu.gameserver.GameServer > log/console.log 2>&1
+	java -Xms128m -Xmx1536m -Xbootclasspath/p:libs/jsr166-1.0.0.jar -ea -javaagent:./libs/Users\wennerstrom\.m2\repository\org\aionlightning\al-commons\1.3\al-commons-1.3.jar -cp ./libs/*:AL-Game.jar com.aionemu.gameserver.GameServer > log/console.log 2>&1
 	err=$?
 	gspid=$!
 	echo ${gspid} > gameserver.pid
